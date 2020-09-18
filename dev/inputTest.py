c49e1c6f11b5a5693c93ed9c3a45eff60164331c
5am5ecure <66415152+5am5ecure@users.noreply.github.com>
2020-09-18 18:55:12 +0100

    Update inputTest.py

--- dev/inputTest.py
+++ dev/inputTest.py
@@ -1,1 +1,18 @@
+import cgi
+
 print("Testing")
+
+form = cgi.FieldStorage()
+
+first_input=form.getvalue('fInput')
+second_input=form.getvalue('sInput')
+
+print("Content-type:text/html\r\n\r\n")
+print ("<html>")
+print ("<head>")
+print ("<title>Testing - Python CGI Script</title>")
+print ("</head>")
+print ("<body>")
+print ("<h1>Inputted strings: {} {}</h1>".format(first_input, second_input))
+print ("</body>")
+print ("</html>")
